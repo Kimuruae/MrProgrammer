d06e1734ca69ef187aab221c230c9d1ba98da772
Edwin Kimuruae <sururu.kimuruae@kimuruae.edu>
2024-08-08 17:15:57 +0300

    Blog page

---
+++ blog.php
@@ -0,0 +1,32 @@
+<?php include_once("templates/header.php");?>
+<?php include_once("templates/nav.php");?>
+    <main class="content container mt-5">
+        <h1>My Blog</h1>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="blog-post p-4 border rounded mb-4">
+                    <h2 class="post-title">Blog Post 1</h2>
+                    <p class="post-date">August 8, 2024</p>
+                    <p class="post-excerpt">This is a short description or excerpt of the blog post...</p>
+                    <a href="post1.html" class="btn btn-primary">Read More</a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="blog-post p-4 border rounded mb-4">
+                    <h2 class="post-title">Blog Post 2</h2>
+                    <p class="post-date">July 15, 2024</p>
+                    <p class="post-excerpt">This is another short description or excerpt of the blog post...</p>
+                    <a href="post2.html" class="btn btn-primary">Read More</a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="blog-post p-4 border rounded mb-4">
+                    <h2 class="post-title">Blog Post 3</h2>
+                    <p class="post-date">June 22, 2024</p>
+                    <p class="post-excerpt">Here’s a short description or excerpt of this blog post...</p>
+                    <a href="post3.html" class="btn btn-primary">Read More</a>
+                </div>
+            </div>
+        </div>
+    </main>
+<?php include_once("templates/footer.php");?>
