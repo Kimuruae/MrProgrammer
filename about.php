f69514c0a59198c1f94bbb13e75a9e4b578c546c
Edwin Kimuruae <sururu.kimuruae@kimuruae.edu>
2024-08-08 17:12:50 +0300

    About page

---
+++ about.php
@@ -0,0 +1,8 @@
+    <?php include_once("templates/header.php");?>
+    <?php include_once("templates/nav.php");?>
+    <main class="content">
+        <h1>About Me</h1>
+        <p>I am a passionate web developer with experience in creating dynamic and responsive websites...</p>
+    </main>
+  <?php include_once("Templates/footer.php");?>
+
