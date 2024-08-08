13d224acfd88f7c9750027f04fd2675063baa377
Edwin Kimuruae <sururu.kimuruae@kimuruae.edu>
2024-08-08 17:36:25 +0300

    templates

---
+++ templates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About - Edwin Kimuruae</title>
+    <!-- Bootstrap CSS for a responsive webpage -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS link -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
---
+++ templates/nav.php
@@ -0,0 +1,28 @@
+    <!--Navaigation bar-->
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Edwin Kimuruae</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">   <a class="nav-link" href="index.php">Home</a>
+                <li class="nav-item">
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="services.php">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link"  href="portfolio.php">Portfolio</a>
+                </li> 
+                <li class="nav-item">
+                    <a class="nav-link" href="blog.php">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
