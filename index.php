5adbbed7792310f0aadec15d07ce78345683b732
Edwin Kimuruae <sururu.kimuruae@kimuruae.edu>
2024-08-08 17:19:18 +0300

    home page

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include_once("templates/header.php");?>
+<?php include_once("templates/nav.php");?>
+    <main class="content">
+        <h1>Hi, I'm Edwin Kimuruae</h1>
+        <h3>I am a Web Developer</h3>
+        <p>Welcome to my personal website!</p>
+        <div class="btn-box">
+            <a href="#">Hire me</a>
+            <br>
+            <br>
+            <a href="contact.html">Let's Talk</a>
+        </div>
+    </main>
+    <?php include_once("templates/footer.php");?>
+
+
