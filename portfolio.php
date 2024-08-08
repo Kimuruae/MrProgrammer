b3f6714d012a4f2f1e4418f1f6327cb5127bc341
Edwin Kimuruae <sururu.kimuruae@kimuruae.edu>
2024-08-08 17:21:29 +0300

    Portfolio page

---
+++ portfolio.php
@@ -0,0 +1,34 @@
+<?php include_once("templates/header.php");?>
+<?php include_once("templates/nav.php");?>
+<main class="content container mt-5">
+    <h1>My Portfolio</h1>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="portfolio-item p-4 border rounded mb-4">
+                <img src="images/project1.jpg" alt="Project 1" class="img-fluid">
+                <h3 class="mt-3">Project 1</h3>
+                <p>Description of Project 1.</p>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="portfolio-item p-4 border rounded mb-4">
+                <img src="images/project2.jpg" alt="Project 2" class="img-fluid">
+                <h3 class="mt-3">Project 2</h3>
+                <p>Description of Project 2.</p>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="portfolio-item p-4 border rounded mb-4">
+                <img src="images/project3.jpg" alt="Project 3" class="img-fluid">
+                <h3 class="mt-3">Project 3</h3>
+                <p>Description of Project 3.</p>
+            </div>
+            </div>
+            </div>
+            <!-- Add more portfolio items as needed -->
+        </div>
+            <!-- Add more portfolio items as needed -->
+    </div>
+</main>
+<?php include_once("templates/footer.php");?>
+
